875cb48430cedb56e9ed7bcd232a9948ead6baa1
Rohyer <guilhermerl.dev@gmail.com>
2024-01-11 23:20:44 -0300

    Develop App component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Nav />
+    </>
+  )
+}
+
+export default App
